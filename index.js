d6d64c5df454b81a8f87f4775a88292dfea15f93
Nilay Chikani <nilaychikani@Nilays-Air.bbrouter>
2023-03-21 14:08:46 +0530

    api changes

--- index.js
+++ index.js
@@ -1,60 +1,76 @@
-const mongoose = require("mongoose");
-const util = require("util");
-const debug = require("debug")("node-server:index");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const compress = require("compression");
+const methodOverride = require("method-override");
+const cors = require("cors");
+const httpStatus = require("http-status");
+const { ValidationError } = require("express-validation");
+const helmet = require("helmet");
+// const routes = require("./routes");
+// const APIError = require("./helpers/APIError");
 
-// config should be imported before importing any other file
-// const config = require("./src/config");
-const server = require("./src/server");
+const app = express();
 
-// // make bluebird default Promise
-// Promise = require("bluebird"); // eslint-disable-line no-global-assign
+// parse body params and attache them to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// // plugin bluebird promise in mongoose
-// mongoose.Promise = Promise;
+app.use(cookieParser());
+app.use(compress());
+app.use(methodOverride());
 
-// // connect to mongo db
-// const mongoUri =
-//   "mongodb+srv://chikaninilay:Vardhman1411@sadharmik-database.phwj3pu.mongodb.net/?retryWrites=true&w=majority";
-// mongoose.connect(mongoUri, {
-//   useCreateIndex: true,
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   promiseLibrary: Promise,
-//   useFindAndModify: false,
-// });
+// secure apps by setting various HTTP headers
+app.use(helmet());
 
-// mongoose.connection.on("error", () => {
-//   throw new Error(`unable to connect to database: ${mongoUri}`);
-// });
-
-// // print mongoose logs in dev env
-// // if (config.mongooseDebug) {
-// //   mongoose.set("debug", (collectionName, method, query, doc) => {
-// //     debug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc);
-// //   });
-// // }
-
-// // module.parent check is required to support mocha watch
-// // src: https://github.com/mochajs/mocha/issues/1912
-// if (!module.parent) {
-//   // listen on port config.port
-//   server.listen("4000", () => {
-//     debug("server started on port 4000");
-//   });
-// }
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
 
 const PORT = 4000;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `);
 });
 
-server.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hey this is my API running 🥳");
 });
 
-server.get("/about", (req, res) => {
+app.get("/about", (req, res) => {
   res.send("This is my about route..... ");
 });
 
-module.exports = server;
+// mount all routes on /api path
+// app.use("/api", routes);
+
+// // if error is not an instanceOf APIError, convert it.
+// app.use((err, req, res, next) => {
+//   if (err instanceof ValidationError) {
+//     // validation error contains details object which has error message attached to error property.
+//     const allErrors = err.details.map((pathErrors) =>
+//       Object.values(pathErrors).join(", ")
+//     );
+//     const unifiedErrorMessage = allErrors
+//       .join(", ")
+//       .replace(/, ([^,]*)$/, " and $1");
+//     const error = new APIError(unifiedErrorMessage, err.statusCode);
+//     return next(error);
+//   }
+//   if (!(err instanceof APIError)) {
+//     const apiError = new APIError(err.message, err.status);
+//     return next(apiError);
+//   }
+//   return next(err);
+// });
+
+// // catch 404 and forward to error handler
+// app.use((req, res, next) => {
+//   const err = new APIError("API Not Found", httpStatus.NOT_FOUND);
+//   return next(err);
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("Hey this is my API running 🥳");
+// });
+
+module.exports = app;
