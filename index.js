bac1d344565ca634798d4047d3c6a448374ff563
Nilay Chikani <nilaychikani@Nilays-Air.bbrouter>
2023-03-21 15:25:18 +0530

    api changes

--- index.js
+++ index.js
@@ -19,18 +19,18 @@ app.listen(PORT, () => {
 });
 
 // parse body params and attache them to req.body
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(cookieParser());
-// app.use(compress());
-// app.use(methodOverride());
+app.use(cookieParser());
+app.use(compress());
+app.use(methodOverride());
 
-// // secure apps by setting various HTTP headers
-// app.use(helmet());
+// secure apps by setting various HTTP headers
+app.use(helmet());
 
-// // enable CORS - Cross Origin Resource Sharing
-// app.use(cors());
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
 
 // mount all routes on /api path
 // app.use("/api", routes);
--- src/routes.js
+++ src/routes.js
@@ -11,33 +11,33 @@ const router = express.Router();
 router.get("/health-check", (req, res) => res.send("OK"));
 
 // mount auth routes at /auth
-router.use("/auth", authRoutes);
+// router.use("/auth", authRoutes);
 
-// Validating all the APIs with jwt token.
-router.use(
-  expressJwt({
-    secret: "Nilay",
-    algorithms: ["HS256"],
-    resultProperty: "locals.session",
-    getToken: function fromHeaderOrQuerystring(req) {
-      if (
-        req.headers.authorization &&
-        req.headers.authorization.split(" ")[0] === "Bearer"
-      ) {
-        return req.headers.authorization.split(" ")[1];
-      }
-      if (req.query && req.query.token) {
-        return req.query.token;
-      }
-      return null;
-    },
-  })
-);
+// // Validating all the APIs with jwt token.
+// router.use(
+//   expressJwt({
+//     secret: "Nilay",
+//     algorithms: ["HS256"],
+//     resultProperty: "locals.session",
+//     getToken: function fromHeaderOrQuerystring(req) {
+//       if (
+//         req.headers.authorization &&
+//         req.headers.authorization.split(" ")[0] === "Bearer"
+//       ) {
+//         return req.headers.authorization.split(" ")[1];
+//       }
+//       if (req.query && req.query.token) {
+//         return req.query.token;
+//       }
+//       return null;
+//     },
+//   })
+// );
 
-// mount user routes at /users
-router.use("/users", userRoutes);
+// // mount user routes at /users
+// router.use("/users", userRoutes);
 
-// mount book routes at /books
-router.use("/books", bookRoutes);
+// // mount book routes at /books
+// router.use("/books", bookRoutes);
 
 module.exports = router;
