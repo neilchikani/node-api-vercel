f2a6fd7dc2d0a5e35a5b110fa30eae8401f99bb6
Nilay Chikani <nilaychikani@Nilays-Air.bbrouter>
2023-03-21 13:07:33 +0530

    first commit

--- index.js
+++ index.js
@@ -1,45 +1,20 @@
-const mongoose = require('mongoose');
-const util = require('util');
-const debug = require('debug')('node-server:index');
+// index.js
+const express = require("express");
 
-// config should be imported before importing any other file
-const config = require('./src/config');
-const server = require('./src/server');
+const app = express();
+const PORT = 4000;
 
-// make bluebird default Promise
-Promise = require('bluebird'); // eslint-disable-line no-global-assign
-
-// plugin bluebird promise in mongoose
-mongoose.Promise = Promise;
-
-// connect to mongo db
-const mongoUri = config.mongo.host;
-mongoose.connect(mongoUri, {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  promiseLibrary: Promise,
-  useFindAndModify: false,
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
 });
 
-mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${mongoUri}`);
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
 });
 
-// print mongoose logs in dev env
-if (config.mongooseDebug) {
-  mongoose.set('debug', (collectionName, method, query, doc) => {
-    debug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc);
-  });
-}
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
+});
 
-// module.parent check is required to support mocha watch
-// src: https://github.com/mochajs/mocha/issues/1912
-if (!module.parent) {
-  // listen on port config.port
-  server.listen(config.port, () => {
-    debug(`server started on port ${config.port} (${config.env})`);
-  });
-}
-
-module.exports = server;
+// Export the Express API
+module.exports = app;
